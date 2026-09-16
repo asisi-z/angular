02ff9a45a08c774ae2c062f8268b9ae1ea80109c
asisi-z <azizi.ribeiro@ba.estudante.senai.br>
2026-09-16 09:55:27 -0300

    Enviando projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path: "login", component:Login},
+    {path: "dashboard", component:Dashboard},
+    {path: "home", component:Home}
+];
+
