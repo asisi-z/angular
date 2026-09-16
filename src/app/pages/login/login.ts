c1034f7f26ffdb70a4f5d00078b12d02c4605433
asisi-z <azizi.ribeiro@ba.estudante.senai.br>
2026-09-16 10:12:15 -0300

    atualizando caminhos de imagem e ajustando o modelo de usuário no login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Auth } from '../../services/auth'; 
-import { Router } from '@angular/router'; 
-import { CommonModule } from '@angular/common'; 
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -12,22 +12,22 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class Login {
-usuario = { 
-nome: '',
-senha: '' 
-} 
-mensagemDeErro: String | null = null; 
-constructor(private auth: Auth, private router: Router) {} 
+  usuario = {
+    nome: '',
+    senha: ''
+  }
+  mensagemDeErro: String | null = null;
+  constructor(private auth: Auth, private router: Router) { }
 
-login() { 
+  login() {
 
-this.auth.login(this.usuario).subscribe({ 
-next: response => { 
-this.router.navigate(['/home']); 
-}, error: (err) => { 
-this.mensagemDeErro = err.error.message || 'Ocorreu um erro ao tentar fazer login.'; 
-} 
-}) 
-} 
+    this.auth.login(this.usuario).subscribe({
+      next: response => {
+        this.router.navigate(['/home']);
+      }, error: (err) => {
+        this.mensagemDeErro = err.error.message || 'Ocorreu um erro ao tentar fazer login.';
+      }
+    })
+  }
 }
 
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';  
 import { Injectable } from '@angular/core';
-import { Usuario } from '../models/usuario';
 import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root',
